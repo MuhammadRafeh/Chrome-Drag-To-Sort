33171f5e51d4817587024bca730dc4355cfd3948
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-07-23 20:14:17 +0500

    On End Gesture, Going back to their positions

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
-import { Dimensions, View } from "react-native";
-import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import { Dimensions, View, StyleSheet } from "react-native";
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { COL, getOrder, getPosition, Positions, SIZE } from "./Config";
@@ -21,10 +21,11 @@ const Item = ({ children, positions, id }: ItemProps) => {
   // const position = getPosition(getOrder(p1.x, p1.y))
   const translateX = useSharedValue(position.x);
   const translateY = useSharedValue(position.y);
+  // const isGestureStart = useSharedValue(false);
   // console.log(positions.value[id], id, position)
 
   const onGestureEvent = useAnimatedGestureHandler({
-    onStart: (event, context: any) => {
+    onStart: (_, context: any) => { //we keep record of already done tranlation because gesture event always start with 0;
       console.log(111111111111111111111, translateX.value)
       context.xOffset = translateX.value
       context.yOffset = translateY.value
@@ -35,32 +36,40 @@ const Item = ({ children, positions, id }: ItemProps) => {
       translateY.value = context.yOffset + event.translationY;
     },
     onEnd: (event, context) => {
-      
+      const firstPosition = getPosition(positions.value[id]);
+      translateX.value = withTiming(firstPosition.x)
+      translateY.value = withTiming(firstPosition.y)
     }
   })
-  
-  
+
+
   const style = useAnimatedStyle(() => {
     return {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: SIZE,
-      height: SIZE,
-      margin: 2,
       transform: [
+        // {scale: isGestureStart.value ? 1.2: 1},
         { translateX: translateX.value },
         { translateY: translateY.value }
       ],
-      backgroundColor: 'red'
+      // backgroundColor: 'red'
     }
   })
   // console.log(positions[id], id)
   //Defining this view in order to translate children's parent view
-  
+
   return <PanGestureHandler onGestureEvent={onGestureEvent}>
-    <Animated.View style={style}>{children}</Animated.View>
+    <Animated.View style={[styles.box, style]}>{children}</Animated.View>
   </PanGestureHandler>
 };
 
 export default Item;
+
+const styles = StyleSheet.create({
+  box: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: SIZE,
+    height: SIZE,
+    margin: 2,
+  }
+})
