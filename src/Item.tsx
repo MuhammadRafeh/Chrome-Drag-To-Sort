b21e852227ccaaa615ec55f89fdbc5df6c1cef0f
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-07-23 19:13:27 +0500

    Implemented reanimated and convert View to Animate

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import { Dimensions, View } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { COL, getOrder, getPosition, Positions, SIZE } from "./Config";
@@ -16,24 +16,28 @@ const Item = ({ children, positions, id }: ItemProps) => {
   const containerHeight =
     Dimensions.get("window").height - inset.top - inset.bottom;
   // const contentHeight = (Object.keys(positions.value).length / COL) * SIZE;
-  const p1 = getPosition(positions.value[id]) //positions[id] is a position;
-  const position = getPosition(getOrder(p1.x, p1.y))
-  console.log(positions.value[id], id, position, getOrder(p1.x, p1.y))
-  const style = { 
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: SIZE,
-    height: SIZE,
-    margin:2,
-    transform: [
-      {translateX: position.x},
-      {translateY: position.y}
-    ],
-    backgroundColor: 'red'
-  }
+  const position = getPosition(positions.value[id]) //positions[id] is a position;
+  // const position = getPosition(getOrder(p1.x, p1.y))
+  const translateX = useSharedValue(position.x);
+  const translateY = useSharedValue(position.y);
+  // console.log(positions.value[id], id, position)
+  const style = useAnimatedStyle(() => {
+    return {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: SIZE,
+      height: SIZE,
+      margin: 2,
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value }
+      ],
+      backgroundColor: 'red'
+    }
+  })
   // console.log(positions[id], id)
-  return <View style={style}>{children}</View>;//Defining this view in order to translate children's parent view
+  return <Animated.View style={style}>{children}</Animated.View>;//Defining this view in order to translate children's parent view
 };
 
 export default Item;
